3080bd10b43e620f27ce4237dbf945b4c2ed4b7e
Arthur Lopes Amorim Faleiro <arthurvc132@gmail.com>
2026-07-27 11:28:00 -0300

    Titulo renomeado

--- ControleEstoque/src/main/java/com/example/controleestoque/HelloApplication.java
+++ ControleEstoque/src/main/java/com/example/controleestoque/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TelaPrincipal.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-        stage.setTitle("");
+        stage.setTitle("Controle de estoque");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
